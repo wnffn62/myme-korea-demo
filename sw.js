2b18c8f6926888ee317694d9883ad95059833f68
wnffn62 <wnffn62@gmail.com>
2026-08-28 01:20:44 +0900

    Refresh customer copy with positive action language

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'myme-korea-v12';
-const ASSETS = ['./index.html', './styles.css?v=12', './app.js?v=12', './manifest.webmanifest', './customer_qr.png'];
+const CACHE = 'myme-korea-v13';
+const ASSETS = ['./index.html', './styles.css?v=13', './app.js?v=13', './manifest.webmanifest', './customer_qr.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
