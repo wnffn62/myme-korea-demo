f3cc5fb0f9558b2f8fb89b9fa74c7d9096a4e509
wnffn62 <wnffn62@gmail.com>
2026-08-28 00:49:17 +0900

    Add dedicated stablecoin remittance experience

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'myme-korea-v8';
+const CACHE = 'myme-korea-v9';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './customer_qr.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))));
